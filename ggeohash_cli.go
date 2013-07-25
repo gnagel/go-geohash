deaaffe024414fb8bec8889f19e92c88b2839a6c
Glenn Nagel <glenn@mercury-wireless.com>
2013-07-25 13:35:02 -0400

    Working on cli

--- ggeohash_cli.go
+++ ggeohash_cli.go
@@ -1,7 +1,41 @@
 package main
 
+import "fmt"
+import "github.com/jessevdk/go-flags"
+import "os"
+import "os/exec"
+import "strings"
 import "./ggeohash"
 
+var opts struct {
+		// Slice of bool will append 'true' each time the option
+		// is encountered (can be set multiple times, like -vvv)
+		Verbose []bool `short:"v" long:"verbose" description:"Show verbose debug information"`
+
+		// Example of automatic marshalling to desired type (uint)
+		Offset uint `long:"offset" description:"Offset"`
+
+		// Example of a callback, called each time the option is found.
+		Call func(string) `short:"c" description:"Call phone number"`
+
+		// Example of a required flag
+		Name string `short:"n" long:"name" description:"A name" required:"true"`
+
+		// Example of a value name
+		File string `short:"f" long:"file" description:"A file" value-name:"FILE"`
+
+		// Example of a pointer
+		Ptr *int `short:"p" description:"A pointer to an integer"`
+
+		// Example of a slice of strings
+		StringSlice []string `short:"s" description:"A slice of strings"`
+
+		// Example of a slice of pointers
+		PtrSlice []*string `long:"ptrslice" description:"A slice of pointers to string"`
+
+		// Example of a map
+		IntMap map[string]int `long:"intmap" description:"A map from string to int"`
+	}
 
 func init() {
 }
